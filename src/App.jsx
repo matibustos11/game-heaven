c9e9dca88875208087dbec2f6d954ef1df9d47a2
Matias Bustos <matibustos1993@gmail.com>
2024-08-21 01:10:42 -0300

    correcciones preentrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Navbar from './components/Navbar/Navbar';
 import './App.css'
+import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
  
   return (
     <>
       <Navbar/>
+      <ItemListContainer greeting={'GAME HEAVEN - LEVEL UP YOUR GAME'} />
     </>
   )
 }

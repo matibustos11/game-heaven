879c1f4b125e24502f5d1559d9674adc4531815a
Matias Bustos <matibustos1993@gmail.com>
2024-10-08 20:08:31 -0300

    Visual home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
 
     return (
 
-        <h1>BIENVENIDO A GAME HEAVEN</h1>
+        <div className="hero-section">
+      <div className="hero-overlay"></div>
+      <h1 className="hero-text">¡Bienvenido a Game Heaven!</h1>
+      <Link to="/tienda" className="cta-button-link">
+        Ingresá a nuestra tienda
+      </Link>
+    </div>
     )
 };
 
